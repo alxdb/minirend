24f1ddc7f9047a7489947283104c1c117b8352e4
Alexander Davidson Bryan <alxdb@protonmail.com>
2020-06-24 11:22:00 +0100

    only setting callbacks on glfw init
    also removed unused current_window variable

--- include/display.hpp
+++ include/display.hpp
@@ -24,11 +24,7 @@ public:
   void make_current() { glfwMakeContextCurrent(glfw_window_ptr); }
 
 private:
-  static void glfw_window_close_callback(GLFWwindow*);
-  static void glfw_window_size_callback(GLFWwindow*, int, int);
-  static void glfw_framebuffer_size_callback(GLFWwindow*, int, int);
   static int display_instances;
-  static GLFWwindow* current_window;
 
   std::pair<int, int> m_window_size;
   std::pair<int, int> m_framebuffer_size;
